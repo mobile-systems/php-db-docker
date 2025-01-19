6d09d8df34e6fd46f5c646e9a25a9090a54dc0eb
Klaus5230 <klaus.wenzler@sympatico.ca>
2025-01-19 11:46:50 -0500

    updated login file

--- pgLogin - Copy.php
+++ pgLogin - Copy.php
@@ -5,12 +5,12 @@
 // The PostgreSQL functions in the previous procedure can only be used with PostgreSQL databases. PDO abstracts database access, and enables you to use code that can handle different types of databases.
 // To connect to PostgreSQL using PDO, follow these steps:
 //     Use the following PHP code to connect to PostgreSQL and select a database. Replace username with your username, password with your password, and dbname with the database name: 
-$host="dpg-cu57vcbv2p9s73a106i0-a.ohio-postgres.render.com";
+$host="localhost";
 $port="5432";
-$dbname="courses_1tjm";
-$user="root";
-$password="gChUqp9tqaMLq3Yn8hDw9pBxjRm5s35m";
-$dsn='pgsql:host=dpg-cu57vcbv2p9s73a106i0-a.ohio-postgres.render.com;port=5432;dbname=courses_1tjm;';
+$dbname="courses";
+$user="postgres";
+$password="root";
+$dsn='pgsql:host=localhost;port=5432;dbname=courses;';
 
 
 try{
